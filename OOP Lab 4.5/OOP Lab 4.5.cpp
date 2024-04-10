c6a392cdae07433bb18f052c59eeab13fe84e1d0
RomanRavlyk <Roman.Ravlyk.RI.2023@lpnu.ua>
2024-04-10 16:03:38 +0300

    dsa

--- OOP Lab 4.5/OOP Lab 4.5.cpp
+++ OOP Lab 4.5/OOP Lab 4.5.cpp
@@ -4,77 +4,78 @@
 using namespace std;
 
 int main() {
-	Money m1;
-	Money m2;
+	Pair* m1 = new Money();
+	Pair* m2 = new Money();
+	Pair* f1 = new Fraction();
+	Pair* f2 = new Fraction();
 
-	cin >> m1;
-	cout << m1;
+	cin >> *dynamic_cast<Money*>(m1);
+	cout << *dynamic_cast<Money*>(m1);
 
-	cin >> m2;
-	cout << m2;
+	cin >> *dynamic_cast<Money*>(m2);
+	cout << *dynamic_cast<Money*>(m2);
 
-	cout << m1 + m2;
-	cout << m1 - m2;
-	cout << m1 * m2;
-	cout << m1 / m2;
-	cout << (m1 /= 2);
-	cout << (m1 *= 2);
+	cout << *dynamic_cast<Money*>(m1) + *dynamic_cast<Money*>(m2);
+	cout << *dynamic_cast<Money*>(m1) - *dynamic_cast<Money*>(m2);
+	cout << (*dynamic_cast<Money*>(m1)) * (*dynamic_cast<Money*>(m2));
+	cout << (*dynamic_cast<Money*>(m1)) / (*dynamic_cast<Money*>(m2));
+	cout << (*dynamic_cast<Money*>(m1) /= 2);
+	cout << (*dynamic_cast<Money*>(m1) *= 2);
 
-
-	cout << "> ";
-	if (m1 > m2) { cout << "yes" << endl; }
-	else cout << "no" << endl;
-	cout << "< ";
-	if (m1 < m2) { cout << "yes" << endl; }
-	else cout << "no" << endl;
-	cout << "== ";
-	if (m1 == m2) { cout << "yes" << endl; }
-	else cout << "no" << endl;
-	cout << ">= ";
-	if (m1 >= m2) { cout << "yes" << endl; }
-	else cout << "no" << endl;
-	cout << "<= ";
-	if (m1 <= m2) { cout << "yes"; }
-	else cout << "no" << endl;
-	cout << "!= ";
-	if (m1 != m2) { cout << "yes" << endl; }
-	else cout << "no" << endl;
-
-	cout << "Real type of Money object 1: " << typeid(m1).name() << endl;
-	cout << "Real type of Money object 2: " << typeid(m2).name() << endl;
-
-	Fraction f1;
-	Fraction f2;
-
-	cin >> f1;
-	cout << f1;
-
-	cin >> f2;
-	cout << f2;
-
-	cout << f1 + f2;
-	cout << f1 - f2;
-	cout << f1 * f2;
 
 	cout << "> ";
-	if (f1 > f2) { cout << "yes" << endl; }
+	if (*dynamic_cast<Money*>(m1) > *dynamic_cast<Money*>(m2)) { cout << "yes" << endl; }
 	else cout << "no" << endl;
 	cout << "< ";
-	if (f1 < f2) { cout << "yes" << endl; }
+	if (*dynamic_cast<Money*>(m1) < *dynamic_cast<Money*>(m2)) { cout << "yes" << endl; }
 	else cout << "no" << endl;
 	cout << "== ";
-	if (f1 == f2) { cout << "yes" << endl; }
+	if (*dynamic_cast<Money*>(m1) == *dynamic_cast<Money*>(m2)) { cout << "yes" << endl; }
 	else cout << "no" << endl;
 	cout << ">= ";
-	if (f1 >= f2) { cout << "yes" << endl; }
+	if (*dynamic_cast<Money*>(m1) >= *dynamic_cast<Money*>(m2)) { cout << "yes" << endl; }
 	else cout << "no" << endl;
 	cout << "<= ";
-	if (f1 <= f2) { cout << "yes"; }
+	if (*dynamic_cast<Money*>(m1) <= *dynamic_cast<Money*>(m2)) { cout << "yes"; }
 	else cout << "no" << endl;
 	cout << "!= ";
-	if (f1 != f2) { cout << "yes" << endl; }
+	if (*dynamic_cast<Money*>(m1) != *dynamic_cast<Money*>(m2)) { cout << "yes" << endl; }
 	else cout << "no" << endl;
 
-	cout << "Real type of Money object 1: " << typeid(f1).name() << endl;
-	cout << "Real type of Money object 2: " << typeid(f2).name() << endl;
+	cout << "Real type of Money object 1: " << typeid(*m1).name() << endl;
+	cout << "Real type of Money object 2: " << typeid(*m2).name() << endl;
+
+	cin >> *dynamic_cast<Fraction*>(f1);
+	cout << *dynamic_cast<Fraction*>(f1);
+
+	cin >> *dynamic_cast<Fraction*>(f2);
+	cout << *dynamic_cast<Fraction*>(f2);
+
+	cout << *dynamic_cast<Fraction*>(f1) + *dynamic_cast<Fraction*>(f2);
+	cout << *dynamic_cast<Fraction*>(f1) - *dynamic_cast<Fraction*>(f2);
+	cout << (*dynamic_cast<Fraction*>(f1)) * (*dynamic_cast<Fraction*>(f2));
+
+	cout << "> ";
+	if (*dynamic_cast<Fraction*>(f1) > *dynamic_cast<Fraction*>(f2)) { cout << "yes" << endl; }
+	else cout << "no" << endl;
+	cout << "< ";
+	if (*dynamic_cast<Fraction*>(f1) < *dynamic_cast<Fraction*>(f2)) { cout << "yes" << endl; }
+	else cout << "no" << endl;
+	cout << "== ";
+	if (*dynamic_cast<Fraction*>(f1) == *dynamic_cast<Fraction*>(f2)) { cout << "yes" << endl; }
+	else cout << "no" << endl;
+	cout << ">= ";
+	if (*dynamic_cast<Fraction*>(f1) >= *dynamic_cast<Fraction*>(f2)) { cout << "yes" << endl; }
+	else cout << "no" << endl;
+	cout << "<= ";
+	if (*dynamic_cast<Fraction*>(f1) <= *dynamic_cast<Fraction*>(f2)) { cout << "yes" << endl; }
+	else cout << "no" << endl;
+	cout << "!= ";
+	if (*dynamic_cast<Fraction*>(f1) != *dynamic_cast<Fraction*>(f2)) { cout << "yes" << endl; }
+	else cout << "no" << endl;
+
+	cout << "Real type of Money object 1: " << typeid(*f1).name() << endl;
+	cout << "Real type of Money object 2: " << typeid(*f2).name() << endl;
+
+	return 0;
 }
